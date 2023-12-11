859b14e57986143e29403ae0b808291a0eb87c3b
Claudia Villarroel <claudia.villarroel@live.com>
2023-12-10 21:27:05 -0300

    :hammer: Desafio 3 Setup ready

--- 03_ConsultasMultiplesTablas/01_Setup.sql
+++ 03_ConsultasMultiplesTablas/01_Setup.sql
@@ -1,3 +1,17 @@
+/*CREACION DE BASE DE DATOS*/
+-- Database: desafio3_Claudia_Villarroel_123
+-- DROP DATABASE IF EXISTS "desafio3_Claudia_Villarroel_123";
+
+CREATE DATABASE "desafio3_Claudia_Villarroel_123"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Chile.1252'
+    LC_CTYPE = 'Spanish_Chile.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
 /*TABLA USUARIOS*/
 CREATE TABLE Usuarios(
     id serial,
@@ -35,3 +49,20 @@ INSERT INTO Posts(titulo, contenido, fecha_creacion, fecha_actualizacion, destac
 ('Post 4', 'No me gusto', '2022-01-15 02:40:00', '2022-01-20 04:50:00', false, 5),
 ('Post 5', 'Me parece bien', '2022-08-11 12:45:10', '2022-09-01 00:25:00', true, 10);
 
+
+/*TABLA COMENTARIOS*/
+CREATE TABLE Comentarios(
+    id serial,
+    contenido text,
+    fecha_creacion timestamp,
+    usuario_id bigint,
+    post_id bigint
+);
+
+/*INSERT TABLA COMENTARIOS*/
+INSERT INTO Comentarios(contenido, fecha_creacion, usuario_id, post_id) VALUES
+('Comentario 1', '2022-01-01 12:25:00', 1, 1),
+('Comentario 2', '2022-02-01 23:05:00', 2, 1),
+('Comentario 3', '2022-04-01 21:50:00', 3, 1),
+('Comentario 4', '2022-01-15 02:40:00', 1, 2),
+('Comentario 5', '2022-08-11 12:45:10', 2, 2); 
